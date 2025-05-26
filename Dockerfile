71de69e931e67eef8083b2124c40e492485b6417
saksham gupta <guptasaksham2510@gmail.com>
2025-05-26 16:01:35 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Dockerfile
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install --upgrade pip && \
+    pip install -r requirements.txt
+
+EXPOSE 5000
+
+CMD ["python", "app.py"]
--- worker/consumer.py
+++ worker/consumer.py
@@ -3,11 +3,39 @@
 import pika
 import json
 import time
+import smtplib
+import os
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
 from models import db, Notification
 from app import app  # Ensure app is imported to initialize db
 
+# Load SMTP credentials from .env
+load_dotenv()
+from_email = os.getenv("SMTP_EMAIL")
+app_password = os.getenv("SMTP_PASS")
+
+
+def send_email(to_email, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = to_email
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(from_email, app_password)
+        server.sendmail(from_email, [to_email], msg.as_string())
+        server.quit()
+        print(f"📨 Email successfully sent to {to_email}")
+    except Exception as e:
+        print(f"❌ Email sending failed: {e}")
+        raise
+
+
 def process_notification(data):
-    fail_times=2
+    fail_times = 2
     with app.app_context():  # Ensure DB operations have Flask context
         notif = Notification.query.get(data['notification_id'])
         if not notif:
@@ -18,14 +46,23 @@ def process_notification(data):
         max_retries = 3
         while retries < max_retries:
             try:
-                print(f"📨 Sending {notif.type} to user {notif.user_id}: {notif.message}")
-                if(retries<fail_times):
+                print(f"📨 Sending {notif.type} to user {notif.user_id}")
+                if retries < fail_times:
                     raise Exception("Simulated failure for testing retries")
+
+                if notif.type == 'email':
+                    send_email(
+                        to_email=notif.user_id,
+                        subject="BSS YUN HI",
+                        body=notif.message
+                    )
+
                 time.sleep(1)  # Simulate sending delay
                 notif.status = 'sent'
                 db.session.commit()
                 print(f"✅ Notification {notif.id} sent successfully.")
                 break
+
             except Exception as e:
                 retries += 1
                 print(f"⚠️ Retry {retries} failed: {e}")
@@ -36,11 +73,13 @@ def process_notification(data):
             db.session.commit()
             print(f"❌ Notification {notif.id} failed after {max_retries} retries.")
 
+
 def callback(ch, method, properties, body):
     print("📬 Received a message from queue.")
     data = json.loads(body)
     process_notification(data)
     ch.basic_ack(delivery_tag=method.delivery_tag)
+
 
 def main():
     try:
@@ -54,6 +93,7 @@ def main():
     except pika.exceptions.AMQPConnectionError as e:
         print("❌ Failed to connect to RabbitMQ. Is the server running?")
         print(e)
+
 
 if __name__ == '__main__':
     main()
